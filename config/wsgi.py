033acd482da4dbe78fed689c2c850c50057ea072
beingsakibshaikh <sakibshaikh1833@gmail.com>
2025-06-29 20:06:04 +0530

    Fix BASE_DIR for WhiteNoise in wsgi.py

--- config/wsgi.py
+++ config/wsgi.py
@@ -11,7 +11,10 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 from whitenoise import WhiteNoise
+from pathlib import Path
 
+
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
